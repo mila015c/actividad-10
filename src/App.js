feb63301d67a6f0454477a520e0797a6ae27173c
mila015c <ch2131908@gmail.com>
2024-08-20 21:34:27 +0200

    palindromo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function printHola(){
-  console.log("hola")
+function esPalindromo(palabra){
+  //Eliminar la palabra y convertir a minusculas
+  palabra = palabra.toLowerCase();
+  //Comparar la palabra con su reverso
+  return palabra === palabra.split("").reverse().join("");
+  
 }
-
+function verificarPalindromo() {
+  const texto = document.getElementById('texto').value;
+  if (esPalindromo(texto)) {
+    alert("Es palindromo");
+  } else {
+    alert("No espalindromo");
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-link">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +32,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          onMouseOver={printHola()}
+          
         >
           Learn React
         </a>
@@ -63,7 +74,26 @@ function App() {
         </tbody>        
         </table>
       </header>
-    </div>
+
+    <p class="title">Que es Babel</p>
+    <p class="parrafo">Babel es un transpilador de JavaScript. Su función principal es convertir el código JavaScript moderno (ES6/ES7 y superior) en una versión más antigua de JavaScript (como ES5) que los navegadores puedan entender. Esto es crucial porque no todos los navegadores soportan las últimas características de JavaScript.</p>
+    <p class="title">Que es WebPack</p>
+    <p class="parrafo">Webpack es un empaquetador de módulos. Su trabajo es tomar todos los módulos de tu aplicación (JavaScript, CSS, imágenes, etc.) y empaquetarlos en uno o más archivos que puedan ser utilizados en un navegador.</p>
+    
+    <body>
+      <h1>Verificador de palindromos</h1>
+      <input
+        type="text"
+        id="texto"
+        placeholder="Ingrse una palabra"
+        />
+      <button
+        onClick={verificarPalindromo}>
+          click aqui!
+          </button>  
+    </body>
+
+        </div>
   );
 }
 
